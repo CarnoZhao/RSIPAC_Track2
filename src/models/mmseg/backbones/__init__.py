a143658dfc61c6d1f9d87bb1d9fd63c3273297fe
CarnoZhao <273806108@qq.com>
2022-08-22 15:34:51 +0800

    add mesa quant pvt

--- src/models/mmseg/backbones/__init__.py
+++ src/models/mmseg/backbones/__init__.py
@@ -1,9 +1,11 @@
 from .mit import MixVisionTransformer
 from .pvt import PyramidVisionTransformerV2
+from .pvt_ms import PyramidVisionTransformerV2MS
 from .coat import CoaT
 
 BACKBONES = {_.__name__: _ for _ in [
     MixVisionTransformer,
     PyramidVisionTransformerV2,
+    PyramidVisionTransformerV2MS,
     CoaT,
 ]}
